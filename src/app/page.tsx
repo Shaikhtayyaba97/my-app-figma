0239c4191a631932fcdefe59b98b1c15d933dc87
tayyaba <tayyabashahbaz2020@gmail.com>
2024-11-23 17:30:16 +0500

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Figma(){
   return(
     <div className="flex  flex-row items-center p-4 w-[1920px] h-[92px]  absolute
     justify-between  " style={{backgroundColor: 'rgba(4,56,115,1'}}>
-      
+      <h1>TESTING</h1>
 <div className="font-inter text-2xl font-bold leading-[33.89px] text-left underline decoration-skip-ink-none w-[191px] h-[34px] flex flex-row text-white "><img src="Logo icon.png" alt="" width="37px" height="27px" /> Whitespace</div>
 <ul className="flex flex-row items-center p-0 gap-[60px] mx-auto w-[988.5px] h-[60px]"> 
   <li>Products</li>
